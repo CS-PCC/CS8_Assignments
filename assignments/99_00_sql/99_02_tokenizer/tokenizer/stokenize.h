cd2bc2578d57da5463d11655809ab86f26b7a9b5
Marco Miralles <marco.miralles@gmail.com>
2023-04-29 12:48:27 -0700

    moved changes to .cpp file

--- assignments/99_00_sql/99_02_tokenizer/tokenizer/stokenize.h
+++ assignments/99_00_sql/99_02_tokenizer/tokenizer/stokenize.h
@@ -43,8 +43,4 @@ private:
   
 };
 
-// initializing static member
-// see: https://en.cppreference.com/w/cpp/language/static
-int STokenizer::_table[MAX_ROWS][MAX_COLUMNS];
-
 #endif // STOKENIZE_H
